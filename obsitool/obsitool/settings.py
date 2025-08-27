253678ddc2e1270cd0b4409d07a129e3b8769d80
SatoshiOhashi1998 <nagaomasaki.lauren@gmail.com>
2025-08-27 21:14:43 +0900

    refactor: common.

--- obsitool/obsitool/settings.py
+++ obsitool/obsitool/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # Static files
 # -----------------------------
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 # -----------------------------
 # Media files
